d8557d7395f91be53ed7046831af6894c8a02424
Shawn Faber <sfaber02@gmail.com>
2022-02-03 14:18:34 -0500

    second attempt started

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const findUniq = (array) => {
-    return 'BbBb';
+    let charSet = array[0].toLowerCase().split('').sort().join('');
+
+    //check if each string has char from set
+    if(charSet.length >= 1){
+        for(let i = 1; i < array.length; i++){
+            curr = array[i].toLowerCase().split('').sort().join('').trim().split('');
+            for(char of curr){
+                if(!(charSet.includes(char.toLowerCase()))){
+                    return array[i];
+                }
+            }
+        }
+        return array[0];
+    }else{
+        for(let i = 1; i < array.length; i++){
+            curr = array[i].toLowerCase().split('').sort().join('').trim().split('');
+            if(curr.length > 0){
+                return array[i];
+            }
+        }
+        return array[0];
+    }
 }
 
 
 
-
+// findUniq([ 'Tom Marvolo Riddle', 'I am Lord Voldemort', 'Harry Potter' ]);
+findUniq([ 'foobar', 'barfo', 'fobara', '   ', 'fobra', 'oooofrab' ]);
+// findUniq([ '   ', 'foobar', 'barfo', 'fobara', 'fobra', 'oooofrab' ]);
 
 module.exports = {
     findUniq
